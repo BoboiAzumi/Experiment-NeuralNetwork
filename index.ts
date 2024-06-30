81591170bb59787454d0db5024851a79aeba1cdb
BoboiAzumi <untukherokudeploy@gmail.com>
2024-06-30 20:12:30 +0700

    Update

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { IrisSetosaDetect } from "./iris_setosa_detect";
 const Model: IrisSetosaDetect = new IrisSetosaDetect()
 
 
-let input = [ 5.1, 3.5, 1.4, 0.2 ]
+let input = [ 7, 3.3, 1.4, 0.2 ]
 console.log("Input : ")
 console.log(input)
 console.log("Output : ")
